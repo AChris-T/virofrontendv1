a419fdfc7876c675479e79e20b1e06e0bdd495b9
Akinfenwa Taiwo Christopher <101477417+AChris-T@users.noreply.github.com>
2026-03-28 16:42:17 +0100

    feat:made some adjustment

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: `${process.env.API_BASE_URL}/:path*`,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'i.pravatar.cc' },
